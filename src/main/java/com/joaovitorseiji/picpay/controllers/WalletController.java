957ebf0b8a253c5e2ff7291a174fcf100637e8d2
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-09-29 21:20:41 -0300

    Validacao de Dados de Entrada Feito

--- src/main/java/com/joaovitorseiji/picpay/controllers/WalletController.java
+++ src/main/java/com/joaovitorseiji/picpay/controllers/WalletController.java
@@ -10,13 +10,15 @@ import com.joaovitorseiji.picpay.dtos.CreateWalletDto;
 import com.joaovitorseiji.picpay.entity.Wallet;
 import com.joaovitorseiji.picpay.services.WalletService;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class WalletController {
 	@Autowired
 	private WalletService walletService;
 	
 	@PostMapping("/wallets")
-	public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletDto dto){
+	public ResponseEntity<Wallet> createWallet(@RequestBody @Valid CreateWalletDto dto){
 		var wallet = walletService.createWallet(dto);
 		
 		return ResponseEntity.ok(wallet);
--- src/main/java/com/joaovitorseiji/picpay/dtos/CreateWalletDto.java
+++ src/main/java/com/joaovitorseiji/picpay/dtos/CreateWalletDto.java
@@ -3,8 +3,15 @@ package com.joaovitorseiji.picpay.dtos;
 import com.joaovitorseiji.picpay.entity.Wallet;
 import com.joaovitorseiji.picpay.entity.WalletType;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 //Balance e Id não são necessários, o Backend faz a gerência
-public record CreateWalletDto (String fullname, String document, String email, String password, WalletType.Enum walletType) {
+public record CreateWalletDto (@NotBlank String fullname, 
+							   @NotBlank String document, 
+							   @NotBlank String email, 
+							   @NotBlank String password, 
+							   @NotNull WalletType.Enum walletType) {
 
 	public Wallet toWallet() {
 			return new Wallet(this.fullname, this.document, this.email, this.password, walletType.get());
